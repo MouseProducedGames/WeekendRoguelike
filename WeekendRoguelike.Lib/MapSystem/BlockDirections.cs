1374162b3e175f48f82399c4daef867f0eb8c3cd
MouseProducedGames <largemouse@gmail.com>
2017-04-18 02:20:05 -0700

    Movement blocking enabled.

---
+++ WeekendRoguelike.Lib/MapSystem/BlockDirections.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WeekendRoguelike.MapSystem
+{
+    [Flags]
+    public enum BlockDirections
+    {
+        None = 0,
+        Cardinal = 1,
+        Diagonal = 0x2
+    }
+
+    public static class BlocksDirectionExtensions
+    {
+        #region Public Methods
+
+        public static BlockDirections Match(this BlockDirections match, BlockDirections with)
+        {
+            return match & with;
+        }
+
+        public static bool MatchAll(this BlockDirections match, BlockDirections with)
+        {
+            return match.Match(with) == with;
+        }
+
+        public static bool MatchAny(this BlockDirections match, BlockDirections with)
+        {
+            return match.Match(with) != BlockDirections.None ||
+                with == BlockDirections.None;
+        }
+
+        #endregion Public Methods
+    }
+}
